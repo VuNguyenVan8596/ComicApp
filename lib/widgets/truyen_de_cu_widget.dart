fa758e6dd7f2a757d2419ed2645a4d4f42d28a4d
Văn Vũ Nguyễn <vu.nguyen@alta.com.vn>
2021-07-19 22:33:40 +0700

    add permission internet

--- lib/widgets/truyen_de_cu_widget.dart
+++ lib/widgets/truyen_de_cu_widget.dart
@@ -29,7 +29,7 @@ class TruyenDeCuState extends State {
     print(lstTruyenDecu.length);
     print(item);
     if (op == '+') {
-      if (item > lstTruyenDecu.length - 3) {
+      if (item > lstTruyenDecu.length - 2) {
         setState(() {
           item = 0;
         });
